8255745469c4608fe69584d37c02b6254da1d4ac
Mauro Romero <mau1408@outlook.com>
2022-10-30 18:36:52 -0300

    Modulo_persona_creado

---
+++ modulo_persona.py
@@ -0,0 +1,8 @@
+class persona:
+    def __init__(self,nombre,apellido):
+        self.nombre = nombre
+        self.apellido= apellido
+
+
+    def __str__(self) -> str:
+        return self.nombre + " " + self.apellido
